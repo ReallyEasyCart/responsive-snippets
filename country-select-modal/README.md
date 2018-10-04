251ddc92db3cfa7c31b6ab1902d91e4a44a15fa2
James McKinnon <james.mckinnon@wildfireinternet.co.uk>
2018-10-04 16:50:24 +0100

    feat: Updated Country Select Modal files and readme

--- country-select-modal/README.md
+++ country-select-modal/README.md
@@ -1,3 +1,17 @@
 # Custom Code
 
 This is the exact code that we used to create a GDPR compliant country select modal for Forge.
+
+## How to Install
+
+1. Include the CSS and all Javascript files in the head like so:
+```html
+<link rel="stylesheet" href="/country-select.css">
+<script src="/modal.js"></script>
+<script src="/countries-to-continents.js"></script>
+<script src="/country-select-modal.js"></script>
+```
+
+2. Edit the configuration block at the top of country-select-modal.js to point to the images and change current_country to Asia.
+
+3. Search country-select-modal.js for https://pro.ip-api.com/?key=XXXXXXXXXX and replace the Xs with the API Key
--- country-select-modal/js/country-select-modal.js
+++ country-select-modal/js/country-select-modal.js
@@ -1,42 +1,53 @@
-{% include "js/countries-to-continents.js" %}
-
 jQuery(function ($) {
-
-    var shouldShowCountrySelectModal = function (tld, continentCode) {
+    
+    var config = {
+        current_country: 'UK', // UK, US or ASIA
+        uk_flag: '/userfiles/images/website_imgs/uk_flag.png',
+        uk_title: 'UK',
+        uk_url: 'http://www.forgeuk.recds12.co.uk',
+        us_flag: '/userfiles/images/website_imgs/us_flag.png',
+        us_title: 'US',
+        us_url: 'http://www.forgeus.recds12.co.uk',
+        asia_flag: '/userfiles/images/website_imgs/asia_flag.png',
+        asia_title: 'Asia',
+        asia_url: 'http://www.forgemotorsport.asia'
+    };
+    
+    var shouldShowCountrySelectModal = function (continentCode) {
         // If the IP check says that the user is from Europe or Africa and the site they're on 
         // does not end in .co.uk, show the modal.
-        if ((continentCode == 'EU' || continentCode == 'AF') && tld != '.co.uk') {
+        if ((continentCode == 'EU' || continentCode == 'AF') && config.current_country != 'UK') {
             return true;
         }
 
         // If the IP check says that the user is from North or South America and the site
         // they're on does not end in .com, show the modal
-        if ((continentCode == 'NA' || continentCode == 'SA') && tld != '.com') {
+        if ((continentCode == 'NA' || continentCode == 'SA') && config.current_country != 'US') {
             return true;
         }
 
         // If the IP check says that the user is from Asia or Oceania and the site they're on
         // does not end in .asia, show the modal
-        if ((continentCode == 'AS' || continentCode == 'OC') && tld != '.asia') {
+        if ((continentCode == 'AS' || continentCode == 'OC') && config.current_country != 'ASIA') {
             return true;
         }
 
         return false;
     };
 
-    var buildCountrySelectModalTitle = function (countryName, tld) {
+    var buildCountrySelectModalTitle = function (countryName) {
         // Build the part of the title that is common between all possibilities
         var title = 'You appear to be from ' + countryName + ' but you\'ve arrived at our ';
 
-        if (tld == '.co.uk') {
+        if (config.current_country == 'UK') {
             // If the domain ends with .co.uk its the European Site
-            title += 'UK site.';
-        } else if (tld == '.com') {
+            title += config.uk_title + ' site.';
+        } else if (config.current_country == 'US') {
             // If the domain ends with .com its the American Site
-            title += 'American site.';
-        } else if (tld == '.asia') {
+            title += config.us_title + ' site.';
+        } else if (config.current_country == 'ASIA') {
             // If the domain ends with .asia its the Asia Site
-            title += 'Asia site.';
+            title += config.asia_title + ' site.';
         } else {
             // If for some reason there are no matches, set to false to prevent errors
             title = false;
@@ -45,21 +56,21 @@ jQuery(function ($) {
         return title;
     };
 
-    var getCountrySelectModalCurrentSite = function (tld) {
+    var getCountrySelectModalCurrentSite = function () {
         var currentSite = {};
 
-        if (tld == '.co.uk') {
+        if (config.current_country == 'UK') {
             // If the domain ends with .co.uk show the UK Flag and text
-            currentSite.flag = '/userfiles/images/website_imgs/uk_flag.png';
-            currentSite.subText = 'Stay on our UK Site';
-        } else if (tld == '.com') {
+            currentSite.flag = config.uk_flag;
+            currentSite.subText = 'Stay on our ' + config.uk_title + ' Site';
+        } else if (config.current_country == 'US') {
             // If the domain ends with .com show the America Flag and text
-            currentSite.flag = '/userfiles/images/website_imgs/us_flag.png';
-            currentSite.subText = 'Stay on our American Site';
-        } else if (tld == '.asia') {
+            currentSite.flag = config.us_flag;
+            currentSite.subText = 'Stay on our ' + config.us_title + ' Site';
+        } else if (config.current_country == 'ASIA') {
             // If the domain ends with .asia show the Asia Flag and text
-            currentSite.flag = '/userfiles/images/website_imgs/asia_flag.png';
-            currentSite.subText = 'Stay on our Asia Site';
+            currentSite.flag = config.asia_flag;
+            currentSite.subText = 'Stay on our ' + config.asia_title + ' Site';
         }
 
         return currentSite;
@@ -70,19 +81,19 @@ jQuery(function ($) {
 
         if (continentCode == 'EU' || continentCode == 'AF') {
             // If the user is from Europe or Africa show the UK flag, text and use the .co.uk url
-            newSite.flag = '/userfiles/images/website_imgs/uk_flag.png';
-            newSite.subText = 'Go to our UK Site';
-            newSite.url = 'https://www.forgemotorsport.co.uk';
+            newSite.flag = config.uk_flag;
+            newSite.subText = 'Go to our ' + config.uk_title + ' Site';
+            newSite.url = config.uk_url;
         } else if (continentCode == 'NA' || continentCode == 'SA') {
             // If the user is from North or South America show the America flag, text and use the .com url
-            newSite.flag = '/userfiles/images/website_imgs/us_flag.png';
-            newSite.subText = 'Go to our American Site';
-            newSite.url = 'https://www.forgemotorsport.com';
+            newSite.flag = config.us_flag;
+            newSite.subText = 'Go to our ' + config.us_title + ' Site';
+            newSite.url = config.us_url;
         } else if (continentCode == 'AS' || continentCode == 'OC') {
             // If the user is from Asia or Oceania show the Asia flag, text and use the .asia url
-            newSite.flag = '/userfiles/images/website_imgs/asia_flag.png';
-            newSite.subText = 'Go to our Asia Site';
-            newSite.url = 'https://www.forgemotorsport.asia';
+            newSite.flag = config.asia_flag;
+            newSite.subText = 'Go to our ' + config.asia_title + ' Site';
+            newSite.url = config.asia_url;
         }
 
         return newSite;
@@ -105,12 +116,10 @@ jQuery(function ($) {
         return false;
     };
 
-    if (window.CookiePolicy.check('preferences')) {
-        $.getJSON("http://ip-api.com/json/?callback=?", function(data) {
-
-            if (!$.cookie('rec-country-select') && !getUrlParameter('overrideCookie')) {
+    if (window.CookiePolicy.check('preferences') || !window.CookiePolicy) {
+        if (!$.cookie('rec-country-select') && !getUrlParameter('overrideCookie')) {
+            $.getJSON("https://pro.ip-api.com/json/?key=XXXXXXXXXX&callback=?", function(data) {
                 // Get the tld of the domain (.co.uk, .com e.t.c) and the country code returned by ip-api.com
-                var tld = getUrlParameter('countrySelectTld') || location.hostname.match(/(\.(?:[a-z]{2,5}\.)?(?:[a-z]{2,5}))$/)[0];
                 var countryCode = getUrlParameter('countrySelectCode') || data.countryCode;
                 
                 // Using the relational objects of countries and continents; get the country name, continent code and continent name
@@ -119,10 +128,10 @@ jQuery(function ($) {
                     continentName = continents[continentCode];
                 
                 // If the country select modal should be shown...
-                if (shouldShowCountrySelectModal(tld, continentCode)) {
+                if (shouldShowCountrySelectModal(continentCode)) {
                     // Build the title and get the details for the current site the user is on and the site that we want them to go to
-                    var title = buildCountrySelectModalTitle(countryName, tld),
-                        currentSite = getCountrySelectModalCurrentSite(tld),
+                    var title = buildCountrySelectModalTitle(countryName),
+                        currentSite = getCountrySelectModalCurrentSite(),
                         newSite = getCountrySelectModalNewSite(continentCode);
                     
                     // If the above variables have been set correctly...
@@ -130,17 +139,17 @@ jQuery(function ($) {
                         // Show the modal
                         var countrySelectModal = new ModalWindow({
                             type: 'country-select',
-                            content: '<p style="margin-bottom: 1em;">' + title + '</p>\
-                            <table style="width: 100%; border: 0; text-align: center;">\
+                            content: '<p style="margin: 1.5em;">' + title + '</p>\
+                            <table style="width: 100%; border: 0; text-align: center; margin-bottom: 1.5em;">\
                                 <tr>\
                                     <td style="width: 50%;">\
                                         <a class="close-country-select-modal" data-action="stay">\
-                                            <img src="' + currentSite.flag + '">\
+                                            <img src="' + currentSite.flag + '" style="max-width: 50%; margin: auto;">\
                                         </a>\
                                     </td>\
                                     <td style="width: 50%;">\
                                         <a class="close-country-select-modal" href="' + newSite.url + '" data-action="' + newSite.url + '">\
-                                            <img src="' + newSite.flag + '">\
+                                            <img src="' + newSite.flag + '" style="max-width: 50%; margin: auto;">\
                                         </a>\
                                     </td>\
                                 </tr>\
@@ -151,7 +160,12 @@ jQuery(function ($) {
                                     <td style="padding: 0;">\
                                         <a class="close-country-select-modal" href="' + newSite.url + '" data-action="' + newSite.url + '">' + newSite.subText + '</a>\
                                     </td>\
-                            </table>'
+                            </table>',
+                            onCloseCallback: function () {
+                                if (!$.cookie('rec-country-select')) {
+                                    $.cookie('rec-country-select', 'stay', { expires: 365 });
+                                }
+                            }
                         });
 
                         // Close the modal if the "stay on current site" flag is clicked
@@ -174,16 +188,18 @@ jQuery(function ($) {
 
                         // We had to add the cookie modal close event manually by putting "REC.Events.publish('cookie-modal-close');"
                         // into cookie-policy.js in the part that says "modal.on('close', function () { ... }"
-                        REC.Events.subscribe('cookie-modal-close', function () {
+                        REC.Events.subscribe('modal.cookie-law.close', function () {
                             countrySelectModal.open();
                         });
                     }
+                } else {
+                    $.cookie('rec-country-select', 'stay', { expires: 365 });
                 }
-            } else if (getUrlParameter('overrideCookie')) {
-                $.cookie('rec-country-select', 'stay', { expires: 365 });
-            } else if ($.cookie('rec-country-select') !== 'stay') {
-                window.location = $.cookie('rec-country-select');
-            }
-        });
+            });
+        } else if (getUrlParameter('overrideCookie')) {
+            $.cookie('rec-country-select', 'stay', { expires: 365 });
+        } else if ($.cookie('rec-country-select') !== 'stay') {
+            window.location = $.cookie('rec-country-select');
+        }
     }
 });
